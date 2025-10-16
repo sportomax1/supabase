b6b61ec647ec888e3fc9dc9a289a0ea1d5775890
sportomax1 <avalancheofnuggets@gmail.com>
2025-10-16 16:40:42 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-  app_passcode: "4427",
-  supabase_url: "https://zapwwhghjjvovddbzdak.supabase.co",
-  supabase_api_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphcHd3aGdoamp2b3ZkZGJ6ZGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MzE1NjUsImV4cCI6MjA3NjIwNzU2NX0.-bghHr-sxswZpH5oJ59TV0qIxosj__UbuXbOcNNIGd4",
-  supabase_service_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphcHd3aGdoamp2b3ZkZGJ6ZGFrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDYzMTU2NSwiZXhwIjoyMDc2MjA3NTY1fQ.QvOoMS1FLIK3OlVf-sJ3WOAA5w5RJ4t-cAk2Wox4a10",
-  tables: ["users", "posts", "comments", "products", "orders"]
-};
